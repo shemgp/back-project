ad21b2b48b89ba1c369a9615fb252a762a1a8c47
Shem Pasamba <shemgp@aiias.edu>
2017-10-24 11:58:30 +0800

    Add infyom generated menu if available

--- src/resources/views/inc/sidebar.blade.php
+++ src/resources/views/inc/sidebar.blade.php
@@ -1,6 +1,11 @@
 <!-- main sidebar -->
 <aside class="main-sidebar">
   <section class="sidebar">
+    @if (view()->exists('layouts.menu'))
+      <ul class="sidebar-menu tree">
+        @include ('layouts.menu')
+      </ul>
+    @endif
     {!! $htmlMenu !!}
   </section>
 </aside> <!-- main sidebar -->
